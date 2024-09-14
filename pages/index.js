3ef609f53880ea544325b63ba4df38bdc6e1ea6f
郑汉荣 <hanrong.zheng@oraimo.com>
2024-09-14 14:26:51 +0800

    Initial

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import localFont from "next/font/local";
 import styles from "@/styles/Home.module.css";
+import Greet from './greet'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,6 +28,7 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
+          <Greet />
           <Image
             className={styles.logo}
             src="https://nextjs.org/icons/next.svg"
